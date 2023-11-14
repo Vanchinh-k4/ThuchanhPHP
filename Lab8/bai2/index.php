0c677741fc787287d7a92d91e84d9b0ef5497be6
Vanchinh2004 <131035236+Vanchinh-k4@users.noreply.github.com>
2023-11-14 18:22:31 +0700

    Add files via upload

---
+++ Lab8/bai2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="mail.php" enctype="multipart/form-data" method="POST">
+<input type="text" class="form-control" name="email" placeholder="Email">
+<input type="text" class="form-control" name="tieude" placeholder="ten">
+<textarea name="content" id="editor" class="form-control"></textarea>
+<input type="file" class="form-control" name="file"  >
+<button type="submit" class="btn btn-primary">Gửi</button>
+
+</body>
+</html>
